b87f390ac144607a0badd9a74cffdc8a05f6ceab
Abdulqoyum6 <abdulqoyum@simplifiedux.com>
2023-05-10 12:20:35 +0100

    update

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,25 +1,35 @@
-<script setup></script>
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    data() {
+        return {
+
+        }
+    }
+})
+</script>
 
 <template>
     <div>
-        <div class="bg-[#009efb] py-8 sticky h-[100%] top-0 left-0 overflow-auto">
+        <div class="bg-[#009efb] hidden lg:block py-8 h-screen left-0 overflow-hidden">
             <h1 class="mb-6 text-white text-center text-3xl font-semibold">EnvaAccord</h1>
-        <div class="flex flex-col items-center mb-10">
-            <div class="w-fit mb-[9px] rounded-[50%] p-[6px] border-[5px] border-b-white border-b-l-white border-t-[#009efb] border-l-[#009efb] border-r-white">
-                <div class="bg-black w-[80px] h-[80px] rounded-[50%]"></div>
+            <div class="flex flex-col items-center mb-10">
+                <div class="w-fit mb-[9px] rounded-[50%] p-[6px] border-[5px] border-b-white border-b-l-white border-t-[#009efb] border-l-[#009efb] border-r-white">
+                    <div class="bg-black w-[80px] h-[80px] rounded-[50%]"></div>
+                </div>
+                <p class="text-white text-md">Admin</p>
+            </div> 
+            <div class="flex flex-col gap-2 text-lg">
+                <router-link to="/" :class="$route.path === '/' ? 'bg-[whitesmoke] text-[#009EFB]' : 'text-white'" class="flex gap-4 items-center cursor-pointer hover:bg-white hover:text-[#009EFB] text-white px-[1.4rem] py-2 text-white">
+                    <div><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M4 21V9l8-6l8 6v12h-6v-7h-4v7H4Z"/></svg></div>
+                    <p class="">Dashboard</p>
+                </router-link>
+                <router-link :to="{name: 'Add-sample'}" :class="$route.path === '/add-sample' ? 'bg-[whitesmoke] text-[#009EFB]' : 'text-white'" class="flex gap-4 items-center cursor-pointer hover:bg-white hover:text-[#009EFB] text-white px-[1.4rem] py-2 text-white">
+                    <div><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M17.5 21h1v-2.5H21v-1h-2.5V15h-1v2.5H15v1h2.5V21Zm.5 2q-2.075 0-3.538-1.463T13 18q0-2.075 1.463-3.538T18 13q2.075 0 3.538 1.463T23 18q0 2.075-1.463 3.538T18 23ZM9 7V5h12v2H9ZM5 20q-.825 0-1.413-.588T3 18q0-.825.588-1.413T5 16q.825 0 1.413.588T7 18q0 .825-.588 1.413T5 20Zm0-6q-.825 0-1.413-.588T3 12q0-.825.588-1.413T5 10q.825 0 1.413.588T7 12q0 .825-.588 1.413T5 14Zm0-6q-.825 0-1.413-.588T3 6q0-.825.588-1.413T5 4q.825 0 1.413.588T7 6q0 .825-.588 1.413T5 8Zm4 11v-2h2.075Q11 17.5 11 18t.075 1H9Zm0-6v-2h9q-1.425 0-2.675.537T13.125 13H9Z"/></svg></div>
+                    <p>Add Samples</p>
+                </router-link>
             </div>
-            <p class="text-white text-md">Admin</p>
-        </div> 
-        <div class="flex flex-col">
-            <router-link to="/" class="flex gap-4 items-center cursor-pointer hover:bg-white hover:text-[#009EFB] text-white px-[1.4rem] py-2 text-white">
-                <div><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M4 21V9l8-6l8 6v12h-6v-7h-4v7H4Z"/></svg></div>
-                <p class="">Dashboard</p>
-            </router-link>
-            <router-link :to="{name: 'Add-sample'}" class="flex gap-4 items-center cursor-pointer hover:bg-white hover:text-[#009EFB] text-white px-[1.4rem] py-2 text-white">
-                <div><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M17.5 21h1v-2.5H21v-1h-2.5V15h-1v2.5H15v1h2.5V21Zm.5 2q-2.075 0-3.538-1.463T13 18q0-2.075 1.463-3.538T18 13q2.075 0 3.538 1.463T23 18q0 2.075-1.463 3.538T18 23ZM9 7V5h12v2H9ZM5 20q-.825 0-1.413-.588T3 18q0-.825.588-1.413T5 16q.825 0 1.413.588T7 18q0 .825-.588 1.413T5 20Zm0-6q-.825 0-1.413-.588T3 12q0-.825.588-1.413T5 10q.825 0 1.413.588T7 12q0 .825-.588 1.413T5 14Zm0-6q-.825 0-1.413-.588T3 6q0-.825.588-1.413T5 4q.825 0 1.413.588T7 6q0 .825-.588 1.413T5 8Zm4 11v-2h2.075Q11 17.5 11 18t.075 1H9Zm0-6v-2h9q-1.425 0-2.675.537T13.125 13H9Z"/></svg></div>
-                <p>Add Samples</p>
-            </router-link>
-        </div>
         </div>
     </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,18 @@
 <script setup>
 import SideBar from '../components/SideBar.vue';
 import Footer from '../components/Footer.vue';
+import Navbar from '../components/Navbar.vue';
 </script>
 
 <template>
-    <div class="flex min-h-screen">
+  <div class="h-screen overflow-hidden">
+    <div class="lg:flex min-h-screen">
       <SideBar class="w-[23vw]" />
-      <div class="w-[77vw]">
+      <div class="w-screen lg:w-[77vw] bg-[whitesmoke] overflow-y-scroll">
+        <Navbar class="shadow" />
         <router-view></router-view>
       </div>
+      <Footer />
     </div>
+  </div>
 </template>
