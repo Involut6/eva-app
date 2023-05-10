371d2a500f701c1cfe5d7b2fff64abfca285e1e7
AbdulHameedi <sanilinity507@gmail.com>
2023-05-10 11:22:07 +0100

    sidenav & footer

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,12 @@
 <script setup>
 import SideBar from '../components/SideBar.vue';
+import Footer from '../components/Footer.vue';
 </script>
 
 <template>
     <div class="flex">
       <SideBar />
       <router-view></router-view>
+      <Footer l/>
     </div>
 </template>
