f91e3f28f419b7072aa89c428ef430f8154c7464
AbdulrahmanLuqman <fal528@dervac.com>
2023-05-11 20:45:33 +0100

    made delete icon function

--- src/components/SamplePage.vue
+++ src/components/SamplePage.vue
@@ -13,6 +13,7 @@ export default defineComponent({
         }
     }
 })
+
 </script>
 
 <template>
@@ -40,9 +41,9 @@ export default defineComponent({
                         <option value="100">100</option>
                       </select>
                     </div>
-                    <input type="text" class="border border-gray-300 rounded-[20px] px-5 py-1 outline-none" placeholder="Show Data....">
+                    <input type="text" class="border border-gray-300 rounded-[20px] px-5 py-1 outline-none" placeholder="Search....">
                 </div>
-                <div class="min-w-[500px] overflow-auto">
+                <div class="relative min-w-[500px] overflow-auto">
                     <table class="w-full bg-gray-100">
                     <thead class="text-white bg-[#009efb] h-[8vh]">
                         <th>Title</th>
@@ -56,8 +57,8 @@ export default defineComponent({
                             <td>{{ row.dateCreated }}</td>
                             <td>{{ row.status }}</td>
                             <td><div class="flex space-x-2 items-center justify-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="text-[#009efb] cursor-pointer" width="25" height="25" viewBox="0 0 24 24"><path fill="currentColor" d="m19.3 8.925l-4.25-4.2L17.875 1.9L22.1 6.125l-2.8 2.8ZM3 21v-4.25l10.6-10.6l4.25 4.25L7.25 21H3Z"/></svg>
-                                <svg xmlns="http://www.w3.org/2000/svg" class="text-[#009efb] cursor-pointer" width="25" height="25" viewBox="0 0 24 24"><path fill="currentColor" d="M7 21q-.825 0-1.413-.588T5 19V6q-.425 0-.713-.288T4 5q0-.425.288-.713T5 4h4q0-.425.288-.713T10 3h4q.425 0 .713.288T15 4h4q.425 0 .713.288T20 5q0 .425-.288.713T19 6v13q0 .825-.588 1.413T17 21H7Zm2-5q0 .425.288.713T10 17q.425 0 .713-.288T11 16V9q0-.425-.288-.713T10 8q-.425 0-.713.288T9 9v7Zm4 0q0 .425.288.713T14 17q.425 0 .713-.288T15 16V9q0-.425-.288-.713T14 8q-.425 0-.713.288T13 9v7Z"/></svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" class="text-[#009efb] cursor-pointer active:text-red" width="25" height="25" viewBox="0 0 24 24"><path fill="currentColor" d="m19.3 8.925l-4.25-4.2L17.875 1.9L22.1 6.125l-2.8 2.8ZM3 21v-4.25l10.6-10.6l4.25 4.25L7.25 21H3Z"/></svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" class="delete-icon text-red-600 cursor-pointer" width="25" height="25" viewBox="0 0 24 24"><path fill="currentColor" d="M7 21q-.825 0-1.413-.588T5 19V6q-.425 0-.713-.288T4 5q0-.425.288-.713T5 4h4q0-.425.288-.713T10 3h4q.425 0 .713.288T15 4h4q.425 0 .713.288T20 5q0 .425-.288.713T19 6v13q0 .825-.588 1.413T17 21H7Zm2-5q0 .425.288.713T10 17q.425 0 .713-.288T11 16V9q0-.425-.288-.713T10 8q-.425 0-.713.288T9 9v7Zm4 0q0 .425.288.713T14 17q.425 0 .713-.288T15 16V9q0-.425-.288-.713T14 8q-.425 0-.713.288T13 9v7Z"/></svg>
                             </div></td>
                         </tr>
                     </tbody>
@@ -68,7 +69,20 @@ export default defineComponent({
         </div>
         </div>
 
+        <!-- when the delete button is clicked -->
+       <div class="flex justify-center absolute top-0 left-0 right-0">
+        <div class="delete-box bg-white w-[50%] h-fit rounded-lg shadow-lg p-5 space-y-3">
+            <p class="text-[20px] w-full">Are you sure you want to delete <span>clicked table</span> sample list</p>
+            <div class="flex justify-between">
+                <p class="text-[19px] h-fit w-fit text-white cursor-pointer bg-[#009efb] px-2 rounded-[10px] active:bg-gray-800">yes</p>
+                <p class="text-[19px] h-fit w-fit text-white cursor-pointer bg-[#009efb] px-2 rounded-[10px] active:bg-gray-800">no</p>
+            </div>
+        </div>
+       </div>
+
     </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+</style>
