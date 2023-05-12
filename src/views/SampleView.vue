c5447e20993cd3d6098ee2f9ffaac86182558385
Abdulqoyum6 <abdulqoyum@simplifiedux.com>
2023-05-12 08:27:30 +0100

    user-page

--- src/views/SampleView.vue
+++ src/views/SampleView.vue
@@ -12,89 +12,8 @@ export default defineComponent({
 </script>
 
 <template>
-    <div class="container mx-auto my-[200px]" :class="showSample ? 'w-[600px]' : 'w-fit'">
-        <div v-if="showSample" class="">
-        <header>
-            Sample Information
-        </header>
-        <div class="content">
-
-            <div class="title-status">
-                <div class="title">
-                    <p>Title</p>
-                    <p class="receive">Blood Sample</p>
-                </div>
-                
-                <div class="status">
-                    <p>Status</p>
-                    <div class="icons">
-                        <p class="receive">Received</p>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><g fill="none" stroke="#009efb" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"><path d="M20 13V5.749a.6.6 0 0 0-.176-.425l-3.148-3.148A.6.6 0 0 0 16.252 2H4.6a.6.6 0 0 0-.6.6v18.8a.6.6 0 0 0 .6.6H14"/><path d="M16 2v3.4a.6.6 0 0 0 .6.6H20m-4 13h6m0 0l-3-3m3 3l-3 3"/></g></svg>
-                    </div>
-                    
-                    <!-- <div class="icons">
-                        <p class="receive">Method Validation</p>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 48 48"><g fill="none" stroke="#009efb" stroke-linecap="round" stroke-linejoin="round" stroke-width="4"><path d="M42 20V39C42 40.6569 40.6569 42 39 42H9C7.34315 42 6 40.6569 6 39V9C6 7.34315 7.34315 6 9 6H30"/><path d="M16 20L26 28L41 7"/></g></svg>
-                    </div> -->
-
-                    <!-- <div class="icons">
-                        <p class="receive">Analysis in Progress</p>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 14 14"><g fill="none" stroke="#009efb" stroke-linecap="round" stroke-linejoin="round"><path d="M7 13.5a6.5 6.5 0 1 1 6.21-8.41M13.5 7v.5"/><path stroke-dasharray=".889 1.778" d="M13.11 9.23a6.51 6.51 0 0 1-2.79 3.36"/><path d="m9.53 13l-.47.18"/></g></svg>
-                    </div> -->
-
-                    <!-- <div class="icons">
-                        <p class="receive">Analysis Completed </p>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 16 16"><path fill="#009efb" d="M14 0H2C.9 0 0 .9 0 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V2c0-1.1-.9-2-2-2zM7 12.414L3.293 8.707l1.414-1.414L7 9.586l4.793-4.793l1.414 1.414L7 12.414z"/></svg>
-                    </div> -->
-
-                    <!-- <div class="icons">
-                        <p class="receive">QA/ QC Ongoing</p>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 14 14"><g fill="none" stroke="#009efb" stroke-linecap="round" stroke-linejoin="round"><path d="M7 13.5a6.5 6.5 0 1 1 6.21-8.41M13.5 7v.5"/><path stroke-dasharray=".889 1.778" d="M13.11 9.23a6.51 6.51 0 0 1-2.79 3.36"/><path d="m9.53 13l-.47.18"/></g></svg>
-                    </div> -->
-
-                    <!-- <div class="icons">
-                        <p class="receive">QA/ QC Completed</p>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 16 16"><path fill="#009efb" d="M14 0H2C.9 0 0 .9 0 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V2c0-1.1-.9-2-2-2zM7 12.414L3.293 8.707l1.414-1.414L7 9.586l4.793-4.793l1.414 1.414L7 12.414z"/></svg>
-                    </div> -->
-                </div>
-            </div>
-
-            <p>Analysis</p>
-            <div class="title-status-1">
-                
-                <div class="title-statuses">
-                    <div class="title">
-                        <p>Ph</p>
-                        <p>Salinity</p>
-                        <p>Ph</p>
-                        <p>Salinity</p>
-                    </div>
-                    
-                    <div class="status">
-                        <div class="icons">
-                        <p>In progress</p>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 14 14"><g fill="none" stroke="#009efb" stroke-linecap="round" stroke-linejoin="round"><path d="M7 13.5a6.5 6.5 0 1 1 6.21-8.41M13.5 7v.5"/><path stroke-dasharray=".889 1.778" d="M13.11 9.23a6.51 6.51 0 0 1-2.79 3.36"/><path d="m9.53 13l-.47.18"/></g></svg>
-                        </div>
-                        <div class="icons">
-                        <p>Completed</p>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 16 16"><path fill="#009efb" d="M14 0H2C.9 0 0 .9 0 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V2c0-1.1-.9-2-2-2zM7 12.414L3.293 8.707l1.414-1.414L7 9.586l4.793-4.793l1.414 1.414L7 12.414z"/></svg>
-                        </div>
-                        <div class="icons">
-                        <p>In progress</p>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 14 14"><g fill="none" stroke="#009efb" stroke-linecap="round" stroke-linejoin="round"><path d="M7 13.5a6.5 6.5 0 1 1 6.21-8.41M13.5 7v.5"/><path stroke-dasharray=".889 1.778" d="M13.11 9.23a6.51 6.51 0 0 1-2.79 3.36"/><path d="m9.53 13l-.47.18"/></g></svg>
-                        </div>
-                        <div class="icons">
-                        <p>Completed</p>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 16 16"><path fill="#009efb" d="M14 0H2C.9 0 0 .9 0 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V2c0-1.1-.9-2-2-2zM7 12.414L3.293 8.707l1.414-1.414L7 9.586l4.793-4.793l1.414 1.414L7 12.414z"/></svg>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <button class="button">Download Result</button>
-        </div>
-        
-        </div>
-        <div v-else>
+    <div class="h-screen w-screen flex items-center bg-[#009EFB70]">
+        <div class="p-5 rounded-lg mx-auto w-fit h-fit bg-white">
             <p class="text-xl font-semibold mb-4 w-full text-center">Logo</p>
             <div class="w-[350px] mb-8 text-center mx-auto">
                 <p class="text-lg font-semibold">Enter Sample ID</p>
@@ -105,107 +24,59 @@ export default defineComponent({
             <button @click="() => showSample = true" :class="id === '' ? 'bg-[#009efb]' : 'bg-gray-300'" class="font-medium text-white p-2 rounded-lg mt-4 w-full text-center">View Sample</button>
         </div>
     </div>
+
+    <!-- Modal -->
+    <div @click.self="showSample = false"  v-if="showSample" class="h-screen w-screen fixed top-0 left-0 flex justify-center items-center bg-opacity-60 bg-black">
+        <div class="p-5 rounded-lg bg-white w-[550px]">
+            <div class="w-full flex justify-between">
+                <div>
+                    <p class="text-2xl font-semibold">Sample Information</p>
+                    <p class="">View the Information of your sample</p>
+                </div>
+                <div @click="showSample = false">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M6.4 19L5 17.6l5.6-5.6L5 6.4L6.4 5l5.6 5.6L17.6 5L19 6.4L13.4 12l5.6 5.6l-1.4 1.4l-5.6-5.6L6.4 19Z"/></svg>
+                </div>
+            </div>
+            <div class="content">
+
+            <div class="flex space-x-8 mt-5">
+                <div class="">
+                    <p class="font-semibold">Title</p>
+                    <p class="">Blood Sample</p>
+                </div>
+                
+                <div class="">
+                    <p class="font-semibold">Status</p>
+                    <div class="flex space-x-2">
+                        <p class="">Received</p>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><g fill="none" stroke="#009efb" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"><path d="M20 13V5.749a.6.6 0 0 0-.176-.425l-3.148-3.148A.6.6 0 0 0 16.252 2H4.6a.6.6 0 0 0-.6.6v18.8a.6.6 0 0 0 .6.6H14"/><path d="M16 2v3.4a.6.6 0 0 0 .6.6H20m-4 13h6m0 0l-3-3m3 3l-3 3"/></g></svg>
+                    </div>
+                </div>
+            </div>
+
+            <div class="mt-4">
+                <p class="mb-2 font-semibold">Analysis</p>                
+                <div class="flex space-x-6">
+                    <div class="flex space-x-2">
+                        <p>pH</p>
+                        <div class="flex items-center space-x-2">
+                            <p>In progress</p>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 14 14"><g fill="none" stroke="#009efb" stroke-linecap="round" stroke-linejoin="round"><path d="M7 13.5a6.5 6.5 0 1 1 6.21-8.41M13.5 7v.5"/><path stroke-dasharray=".889 1.778" d="M13.11 9.23a6.51 6.51 0 0 1-2.79 3.36"/><path d="m9.53 13l-.47.18"/></g></svg>
+                        </div>
+                    </div>
+                    <div class="flex space-x-4">
+                        <p>pH</p>
+                        <div class="flex items-center space-x-4">
+                            <p>In progress</p>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 14 14"><g fill="none" stroke="#009efb" stroke-linecap="round" stroke-linejoin="round"><path d="M7 13.5a6.5 6.5 0 1 1 6.21-8.41M13.5 7v.5"/><path stroke-dasharray=".889 1.778" d="M13.11 9.23a6.51 6.51 0 0 1-2.79 3.36"/><path d="m9.53 13l-.47.18"/></g></svg>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="ml-auto mr-0 w-fit mt-5">
+                <button class="px-3 py-2 rounded-lg bg-gray-300 text-white">Download Result</button>
+            </div>
+            </div>
+        </div>
+    </div>
 </template>
-
-<style scoped>
-
-.hide {
-    display: none;
-}
-
-input {
-    padding: 0.5em;
-    border-radius: 10px;
-    border: 1px solid lightgray;
-}
-
-.title-status  {
-    display: flex;
-    align-items: center;
-    gap: 8em;
-    padding: 1em 0;
-}
-
-.title-statuses {
-    display: flex;
-    gap: 4em;
-    padding: 1em 0;
-}
-
-.title-statuses p {
-    padding: 0.3em 0;
-    /* margin: 1em 0 0 1em; */
-}
-
-.icons {
-    display: flex;
-    align-items: center;
-    gap: 1em;
-}
-
-.title-status p {
-    padding: 0.2em 0;
-}
-
-.receive {
-    font-weight: bold;
-    font-size: 1.1em;
-    margin: 0.2em 0;
-}
-
-.title-status-1 {
-    border: 1px solid lightgray;
-    padding: 0 1em;
-    border-radius: 10px;
-    margin: 1em 0;
-}
-.container {
-    /* icones */
-    box-shadow: 1px 1px 4px 3px lightgray;
-    /* width: 700px; */
-    border-radius: 20px;
-    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
-    padding: 1em 1.2em;
-    /* color: red; */
-}
-
-header {
-    color: #009efb;
-    text-align: center;
-    font-size: 32px;
-    font-weight: bold;
-}
-
-.content {
-    padding: 0.5em 0;
-}
-
-.content p {
-    color: black;
-    border-radius: 10px;
-}
-
-span {
-    font-weight: bold;
-}
-
-.button {
-    padding: 0.8em;
-    /* border: 2px solid #009efb; */
-    background-color: lightgray;
-    color: white;
-    width: 12em;
-    border-radius: 10px;
-    margin: 1em auto 0;
-    display: flex;
-    justify-content: center;
-    font-weight: 600;
-}
-
-button:hover {
-    /* background-color: #009efb; */
-    /* color: white; */
-    font-weight: bold;
-}
-
-</style>
